17e3769edf2fc4b972804dd60c19ff4c07ebec3a
balbino aylagas <balbino_a@hotmail.com>
2022-01-28 22:11:06 -0600

    created sql script file and database file

---
+++ sql_scripts/scripts.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `personinfodb`.`personinfo` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(85) NOT NULL,
+  `age` INT NOT NULL,
+  `salary` INT NOT NULL,
+  PRIMARY KEY (`id`));
+
+INSERT INTO `personinfodb`.`personinfo`(`id`,`name`,`age`,`salary`) VALUES(0,'bal',23,200);
