526db0d51b15191c4fb475edeaf5db1a2af1a356
balbino aylagas <balbino_a@hotmail.com>
2022-01-28 23:18:21 -0600

    added rest of the db commands, finished crud

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from db import createNewPersonCard
+from db import createNewPersonCard, getPersonById, getAllPersonCards, updatePersonCard, deletePersonCard
 
 
 def showMenu():
@@ -25,17 +25,44 @@ while True:
         break
     elif option == 0:
         """create a new person card"""
+        print()
         name = input("name: ")
         age = int(input("age: "))
         salary = int(input("salary: "))
         createNewPersonCard(name, age, salary)
     elif option == 1:
-        pass
+        """search person by id"""
+        id = int(input("id to search: "))
+        personCardDict = getPersonById(id)
+        print(personCardDict)
+        print()
     elif option == 2:
-        pass
+        """show all person cards"""
+        personCardList = getAllPersonCards()
+        for card in personCardList:
+            print(card)
     elif option == 3:
-        pass
+        """update person card"""
+        id = int(input("id of card to update: "))
+        personCardDict = getPersonById(id)
+        print(personCardDict)
+        updateOption = int(input("are you sure to update this card? (0=no, 1=yes): "))
+        if updateOption:
+            name = input("name: ")
+            age = int(input("age: "))
+            salary = int(input("salary: "))
+            updatePersonCard(id, name, age, salary)
+        else:
+            print("update canceled")
     elif option == 4:
-        pass
+        """delete a person card"""
+        id = int(input("id of card to delete: "))
+        personCardDict = getPersonById(id)
+        print(personCardDict)
+        deleteOption = int(input("are you sure to delete this card? (0=no, 1=yes): "))
+        if deleteOption:
+            deletePersonCard(id)
+        else:
+            print("delete canceled")
     else:
         print("app just continues...")
--- db.py
+++ db.py
@@ -24,17 +24,42 @@ def createNewPersonCard(name, age, salary):
         connection.commit()
 
 
-def getPersonById(id):
-    pass
+def getPersonById(id) -> dict:
+    connection = createConnectionToDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM personinfodb.personinfo where id=%s;"
+            data = (id,)
+            cursor.execute(sql, data)
+            result = cursor.fetchone()
+            return result
 
 
-def getAllPersonCards():
-    pass
+def getAllPersonCards() -> list:
+    connection = createConnectionToDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM personinfodb.personinfo;"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+            return result
 
 
 def updatePersonCard(id, name, age, salary):
-    pass
+    connection = createConnectionToDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "UPDATE `personinfodb`.`personinfo` SET `name` = %s, `age` = %s, `salary` = %s WHERE `id` = %s;"
+            data = (name, age, salary, id)
+            cursor.execute(sql, data)
+        connection.commit()
 
 
 def deletePersonCard(id):
-    pass
+    connection = createConnectionToDB()
+    with connection:
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM `personinfodb`.`personinfo` WHERE id=%s;"
+            data = (id,)
+            cursor.execute(sql, data)
+        connection.commit()
